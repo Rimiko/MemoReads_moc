5d4428cabcd3fb1e561d9e96a4fbdddf53a93877
RIMIKO\Rimiko hukumitsu <rf181.0624.t@gmail.com>
2017-06-03 16:41:56 +0800

    20170603 本の検索結果ページを作成

---
+++ amazonapi.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Company-HTML Bootstrap theme</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">    
+    <link href="css/amazonapi.css" rel="stylesheet"> 
+    <link href="css/header.css" rel="stylesheet">
+    <!-- =======================================================
+        Theme Name: Company
+        Theme URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+        Author: BootstrapMade
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->
+  </head>
+  <body>
+  <?php include('header.php'); ?>
+
+    <section id="contact-page">
+    <div class="kabe">
+    <div class="container">
+            <div class="row main">
+                <div class="main-login main-center">
+
+                    <form class="form-horizontal" method="post" action="#">
+
+                        <h3>検索結果</h3>
+                        <div class="form-group">
+                        <label for="avatar" class="cols-sm-2 control-label">本選択</label>
+                        <div class="row">
+                        <div class="cols-sm-12">
+                        <div class="col-xs-4">
+                        <img src="images/background.png" class="img-responsive img-radio">
+                        <button type="button" class="btn btn-primary btn-radio">lion</button>
+                        <input type="checkbox" id="left-item" class="hidden">
+                    </div>
+                    <div class="col-xs-8">
+                    <p><strong>タイトル：</strong></p>
+                    <p><strong>作者：</strong></p>
+                    <p><strong>ジャンル：</strong></p>
+                    <p><strong>詳細：</strong></p>
+                    </div>
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="cols-sm-12">
+                        <div class="col-xs-4">
+                        <img src="images/background.png" class="img-responsive img-radio">
+                        <button type="button" class="btn btn-primary btn-radio">lion</button>
+                        <input type="checkbox" id="left-item" class="hidden">
+                    </div>
+                    <div class="col-xs-8">
+                    <p><strong>タイトル：</strong></p>
+                    <p><strong>作者：</strong></p>
+                    <p><strong>ジャンル：</strong></p>
+                    <p><strong>詳細：</strong></p>
+                    </div>
+                    </div>
+                    </div>
+                    <div class="row">
+                      <div class="cols-sm-12">
+                        <div class="col-xs-4">
+                        <img src="images/background.png" class="img-responsive img-radio">
+                        <button type="button" class="btn btn-primary btn-radio">lion</button>
+                        <input type="checkbox" id="left-item" class="hidden">
+                    </div>
+                    <div class="col-xs-8">
+                    <p><strong>タイトル：</strong></p>
+                    <p><strong>作者：</strong></p>
+                    <p><strong>ジャンル：</strong></p>
+                    <p><strong>詳細：</strong></p>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+
+<form class="form-horizontal">
+<fieldset>
+
+</fieldset>
+</form>
+
+
+
+
+                        <div class="form-group ">
+                            <button type="button" class="btn btn-primary btn-lg btn-block login-button cols-sm-2">Register</button>
+                        </div>
+                    
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    </section><!--/#contact-page-->
+    <section id="partner">
+        <div class="container">
+            <div class="center wow fadeInDown">
+                <h2>Developers</h2>
+                <div class="footer">
+            <div class="container">
+                <!-- <div class="container"> -->
+            <div class="developers">
+                <div class="col-md-3">
+                    <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" src="images/rimiko.png">
+                        <div><img class="image-circle" src="images/rimiko.png"> </div>    
+                        <h2>Rimiko Fukumitsu</h2>
+                        
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
+                        <div><img class="image-circle" src="images/naru.png"></div>    
+                        <h2>Naru<br> Nishimura</h2>
+                        
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" >
+                        <div><img class="image-circle" src="images/atsushi.png"></div>    
+                        <h2>Atsushi Miyamoto</h2>
+                        
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+                        <div><img class="image-circle" src="images/IMG_1696.png"></div>    
+                        <h2>Ayumi <br>Maeda</h2>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-2.1.1.min.js"></script>  
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>  
+    <script src="js/wow.min.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="js/functions.js"></script>
+    <script src="contactform/contactform.js"></script>
+    
+</body>
+</html>
