b4f8bd029cdb2832602cba1f90f6e74c4f6d359e
ayumi-maeda <maeda_peace4096@yahoo.co.jp>
2017-06-07 09:22:08 +0800

    登録後のマイページへボタンコメントアウト

--- thanks.php
+++ thanks.php
@@ -47,7 +47,7 @@ unset($_SESSION['join']);
                         </div>
 
 <a href="top.php" class="rewrite">TOPに戻る</a>
-<a href="top.php" class="btn btn-success">マイページへ</a>
+<!-- <a href="top.php" class="btn btn-success">マイページへ</a> -->
 <!--            <button type="button" class="btn btn-primary btn-lg btn-block login-button cols-sm-2">Register</button>
 
             <button type="button" class="btn btn-primary btn-lg btn-block login-button cols-sm-2">Register</button> -->
