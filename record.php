8b928519e4a34a5b0507abd9350e20fa7d9939be
DESKTOP-C379KDD\kirur <kirurakiru.0524@gmail.com>
2017-06-12 17:31:21 +0800

    いまからインサート

--- record.php
+++ record.php
@@ -1,5 +1,66 @@
+<?php 
+session_start();
+require('dbconnect.php');
+// require('api.php');
+
+
+if (!empty($_POST)) {
+  if ($_REQUEST['bool_select'] == '') {
+    $error['book_select'] == 'blank';
+    # code...
+  }
+  # code...
+}
+  // var_dump($_SESSION['book']);
+
+
+  
+
+  //session
+if(!empty($_SESSION['book'])){
+  $booktitle=$_SESSION['book']['title'];
+  $bookpic=$_SESSION['book']['pic'];
+  $bookauthor=$_SESSION['book']['author'];
+  $bookid=$_SESSION['book']['bookid'];
+
+
+
+  $sql ='SELECT * FROM `books` WHERE `api_id` ='.$_SESSION['book']['bookids'];
+
+
+  $records = mysqli_query($db,$sql) or die(mysqli_error($db));
+  $record = mysqli_fetch_assoc($records);
+
+  if(isset($record)){
+    $b=$record['book_id'];
+  }else{
+
+$sql='INSERT INTO `books`';
+  }
+}
+
+  // var_dump($record);
+
+
+
+// var_dump($_SESSION['book']);
+// var_dump($_POST);
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,43 +87,61 @@
   <?php include('header.php'); ?>
 
   <div id="allbox">
-  <div id="allbox-sub">
+  
 
         <!-- panel preview -->
-        <div class="container">
+      <div class="container">
         
         <div id="b-box">
            <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="form-group">
-                        <label for="concept" class="col-sm-3 control-label">タイトル</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="concept" name="concept">
-                                         <div class="form-group">
-  <label class="col-ms-4 control-label" for="singlebutton"></label>
-  <div class="col-ms-4">
-    <button id="singlebutton" name="singlebutton" class="btn btn-primary">検索</button>
-  </div>
-</div>
-                        </div>
-                    </div>
-                  
+                          <div class="form-group">
 
+  <label>タイトル</label>
+
+   
+  <div class="col-ms-4">
+
+    
+<form method="get" action="record_result.php" name="検索">
+   
   
-                    <div class="form-group">
+  
+   <?php if(empty($_SESSION)): ?>
+     <input id="serch" type="text" name="title" value="">
+    <?php else: ?>
+    <input id="serch" type="text" name="title" value="<?php echo $booktitle; ?>">
+    <?php endif; ?>
+     <button>検索</button>
+<?php if(isset($error['title'])) { ?>
+  <p class="error">*タイトルを入力してください</p>
+<?php } ?>
+
+
+
+ </form>
+ <form method="post" action="record.php" name="記録">
+ 
+                        </div>
+                      
+                    </div>
+                  </div>
+                </div>
+                 <div class="form-group">
                         <label for="status" class="col-sm-4 control-label">感想</label>
                         <div class="col-sm-9">
                         <form class="form-horizontal">
 <fieldset>
 
-<!-- Form Name -->
 
 
 <!-- Text input-->
 <div class="form-group">
   
   <div class="col-ms-4">
-  <textarea id="textinput" name="textinput" type="text" placeholder="book name" class="form-control input-ms" required="">
+  <textarea id="textinput" name="textinput" type="text" class="form-control input-ms" required="">
     </textarea>
   </div>
 </div>
@@ -72,53 +151,53 @@
 
 <!-- Multiple checkbox (inline) -->
 <div class="form-group">
-  <label class="col-ms-4 control-label" for="keyword">キーワード</label>
+  <label>キーワード</label>
   <div class="col-ms-4"> 
     <label class="checkbox-inline" for="keyword-0">
       <input type="checkbox" name="keyword" id="keyword-0" value="1" checked="checked">
       予想外
     </label> 
     <label class="checkbox-inline" for="keyword-1">
-      <input type="checkbox" name="keyword" id="keyword-1" value="2">
+      <input type="checkbox" name="keyword1" id="keyword-1" value="2">
       面白い
     </label> 
     <label class="checkbox-inline" for="keyword-2">
-      <input type="checkbox" name="keyword" id="keyword-2" value="3">
+      <input type="checkbox" name="keyword2" id="keyword-2" value="3">
       笑える
     </label> 
     <label class="checkbox-inline" for="keyword-3">
-      <input type="checkbox" name="keyword" id="keyword-3" value="4">
+      <input type="checkbox" name="keyword3" id="keyword-3" value="4">
       つまらない
     </label> 
     <label class="checkbox-inline" for="keyword-4">
-      <input type="checkbox" name="keyword" id="keyword-4" value="5">
+      <input type="checkbox" name="keyword4" id="keyword-4" value="5">
       びっくり
     </label> 
     <label class="checkbox-inline" for="keyword-5">
-      <input type="checkbox" name="keyword" id="keyword-5" value="6">
+      <input type="checkbox" name="keyword5" id="keyword-5" value="6">
       胸キュン
     </label> 
     <label class="checkbox-inline" for="keyword-6">
-      <input type="checkbox" name="keyword" id="keyword-6" value="7">
+      <input type="checkbox" name="keyword6" id="keyword-6" value="7">
       元気
     </label> 
     <label class="checkbox-inline" for="keyword-7">
-      <input type="checkbox" name="keyword" id="keyword-7" value="8">
+      <input type="checkbox" name="keyword7" id="keyword-7" value="8">
       価値観変わる
     </label> 
     <label class="checkbox-inline" for="keyword-8">
-      <input type="checkbox" name="keyword" id="keyword-8" value="9">
+      <input type="checkbox" name="keyword8" id="keyword-8" value="9">
       とりあえず読め
     </label> 
     <label class="checkbox-inline" for="keyword-9">
-      <input type="checkbox" name="keyword" id="keyword-9" value="10">
+      <input type="checkbox" name="keyword9" id="keyword-9" value="10">
       考えさせられる
     </label>
   </div>
 </div>
 
 </fieldset>
-</form>
+
 
                         </div>
                     </div> 
@@ -131,22 +210,26 @@
                      <div class="form-group">
                         <label for="date" class="col-sm-3 control-label">読み終わり</label>
                         <div class="col-sm-9">
-                            <input type="date" class="form-control" id="date" name="date">
+                            <input type="date" class="form-control" id="date" name="date2">
                         </div>
                     </div>    
                     <div class="form-group">
                         <div class="col-sm-12 text-right">
-                            <button type="button" class="btn btn-default preview-add-button">
-                                <span class="glyphicon glyphicon-plus"></span> Add
-                            </button>
+                            <input type="submit" name="記録" value="記録">
+                            
                         </div>
                     </div>
-                </div>
-            </div>            
-        </div> <!-- / panel preview -->
-    </div>
-</div>
-    </div>
+                
+     <!-- / panel preview -->
+      </form>
+      </div>
+      </div>
+      </div>
+      </div>
+      
+    
+
+
            <section id="partner">
         <div class="container">
             <div class="center wow fadeInDown">
--- record_result.php
+++ record_result.php
@@ -1,13 +1,35 @@
 <?php  
+session_start();
 require('api.php');
 
 
-
+//何も入れずに検索した場合の処理
 if ($_REQUEST['title'] == '') {
-  header('Location: record.php');
+  header('Location: record.php?title=');
   exit();
 
 }
+
+
+if(isset($_REQUEST['book_select'])){
+    $a=$_REQUEST['book_select'];
+    $_SESSION['book']['title']=$titles[$a];
+    $_SESSION['book']['pic']=$pics[$a];
+    $_SESSION['book']['author']=$authors[$a];
+    $_SESSION['book']['bookid']=$bookids[$a];
+
+    header("Location:record.php");
+    exit();
+}
+ // if (empty($error)){
+ //      // セッションに値を保存
+ //     $_SESSION['book'] = $_POST;
+
+ //      //$_SESSION['join']['picture_path'] = $picture_path;
+ //      //リダイレクト処理を実行する関数header()
+ //       header("Location:record.php");
+ //       exit();
+ //      }
 
 ?>
 
@@ -32,16 +54,17 @@ if ($_REQUEST['title'] == '') {
 <div id="a-box">
 <h1>選択</h1>
         
-<form method="post">
-　　　　　　　<?php for ($i=0; $i < $c ; $i++): ?>
+<form method="post" action="">
+<?php for ($i=0; $i < $c ; $i++): ?>
                    
        
                     <div class="portfolio-item bootstrap wordpress col-xs-6 col-sm-6 col-md-6 serch"  style="width: 380px;height: 225px;">
 
                    
 <label>
-      <input type="radio" name="book-select" id="radios-0" value="<?php echo $i ?>" checked="checked">
-            <div class="well well-sm" >
+
+      <input type="radio" name="book_select" id="radios-0" value="<?php echo $i; ?>" checked="checked">
+            <div class="well well-sm">
                 <div class="row">
                     <div class="col-sm-6 col-md-6" style="width: 170px;">
                      <img src="<?php echo $pics[$i]; ?>"  style="width: 100px;height:170px;">
@@ -51,15 +74,14 @@ if ($_REQUEST['title'] == '') {
                     <div class="col-sm-6 col-md-6">
                    
                     <h5>タイトル</h5>
-　　　　　　　　　　　<h5><?php echo  $titles[$i]; ?></h5>
+<h5><?php echo  $titles[$i]; ?></h5>
 <!-- <?php var_dump($authors); ?> -->
 <?php if (!isset($authors[$i])): ?>
     <h5>なし</h5>
           <?php else: ?>
-            <?php foreach ($authors[$i] as $au): ?>　
-　　　　　　　　　　　<h5><?php echo "著者:" .$au; ?></h5>
+            <h5><?php echo "著者:" .$authors[$i][0]; ?></h5>
 
-    <?php endforeach; ?>
+   
      <?php endif; ?>
      </label>
 
