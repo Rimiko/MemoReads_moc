df667cab766d78b7d11301b88eec562511a5f731
ayumi-maeda <maeda_peace4096@yahoo.co.jp>
2017-06-12 22:19:50 +0800

    特に変更なし

--- like_btn_sample.php
+++ like_btn_sample.php
@@ -19,6 +19,7 @@ require('dbconnect.php');
 
 
 
+
 ?>
 
 
--- unlike.php
+++ unlike.php
@@ -4,7 +4,7 @@ require('dbconnect.php');
 
 // get送信されたtweet_idを取得
 if(isset($_REQUEST['record_id'])){
-// SQL文作成（likesテーブルのINSERT文）$_SESSION['login_user_id']と$_REQUEST['record_id']をいれること
+// SQL文作成（likesテーブルのINSERT文）$_SESSION['login_member_id']と$_REQUEST['record_id']をいれること
 $sql = 'DELETE FROM `likes` WHERE `user_id`=2  AND `record_id` ='.$_REQUEST['record_id'];
 //SQL文実行
 mysqli_query($db,$sql) or die(mysqli_error($db));
