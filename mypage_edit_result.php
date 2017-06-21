d5bd479125460dcbc9b5e89e8ff8f86773eda859
DESKTOP-C379KDD\kirur <kirurakiru.0524@gmail.com>
2017-06-21 21:56:56 +0800

    できた

--- mypage_edit_result.php
+++ mypage_edit_result.php
@@ -40,7 +40,7 @@ if(isset($_REQUEST['book_select'])){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>record_result</title>
+    <title>mypage_edit_result</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -51,12 +51,12 @@ if(isset($_REQUEST['book_select'])){
     <link rel="stylesheet" href="css/record_result.css"> 
 
 </head>
-<body>
-<div id="a-box">
-<h1>選択</h1>
-        
+<body id="allbox">
+<div>
+<h1><img src="images/books.PNG">選択してください</h1>        
 <form method="post" action="">
-<?php for ($i=0; $i < $c ; $i++): ?>
+<div class="row">
+<?php for ($i=0; $i < 9; $i++): ?>
                    
        
                     <div class="portfolio-item bootstrap wordpress col-xs-6 col-sm-6 col-md-6 serch"  style="width: 380px;height: 225px;">
@@ -66,12 +66,16 @@ if(isset($_REQUEST['book_select'])){
 
       <input type="radio" name="book_select" id="radios-0" value="<?php echo $i; ?>" checked="checked">
             <div class="well well-sm">
-                <div class="row" style="width:390px; height:170px;">
-                    <div class="col-sm-6 col-md-6" style="width: 170px;">
-                     <img src="<?php echo $pics[$i]; ?>"  style="width: 100px;height:170px;">
+                <div class="books" style="width:390px; height:170px;">
+                     <div class="col-sm-6 col-md-6" style="width: 170px;">
+                    <?php if($pics[$i] == 'なし'): ?>
+                      
+                      <img src="images/noimage.PNG" style="width: 100px;height:170px;">
+                    <?php else: ?>
+                      <img src="<?php echo $pics[$i]; ?>"  style="width: 100px;height:170px;">
+                    <?php endif ?>
+                    
                     </div>
-
-  
                     <div class="col-sm-6 col-md-6">
                    
                     <h5>タイトル</h5>
@@ -87,42 +91,22 @@ if(isset($_REQUEST['book_select'])){
     
 
                     
-                   </div>
-
+                    </div>
                   </div>
-
-                   
-                </div>
-
-      
+                 </div>
+                </label>     
               </div>
 
-              </label>
-
-
-
-
-       
-              
-         
-      
   <?php endfor; ?>
 
   <input type="submit" value="選択">
+  </div>
 
   </form>
+  </div>
 
 
-                  
-  
-
-                  
-
-
-
-
-
-
+            
 
 </body>
 </html>  
