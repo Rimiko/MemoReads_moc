b17f65822404f16a4f895071392eed9d5c2065e6
RIMIKO\Rimiko hukumitsu <rf181.0624.t@gmail.com>
2017-06-22 09:56:04 +0800

    タイトル

--- record.php
+++ record.php
@@ -236,7 +236,7 @@ exit();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Company-HTML Bootstrap theme</title>
+    <title>記録ページ</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
