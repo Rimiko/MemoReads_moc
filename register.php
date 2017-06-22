c3659dd9c255ea302c83a9dcb4628d31b9ffa7e1
ayumi-maeda <maeda_peace4096@yahoo.co.jp>
2017-06-22 09:57:16 +0800

    タイトル変更

--- register.php
+++ register.php
@@ -110,7 +110,7 @@ if(empty($_POST['password_2'])){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Company-HTML Bootstrap theme</title>
+    <title>会員登録ページ</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
